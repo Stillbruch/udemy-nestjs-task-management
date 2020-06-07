e15f6ca2ac2cc8cfce94fa768fd1819ee2f2feee
Alexandre Mai <a.mai.e05@gmail.com>
2020-06-07 18:09:53 +0200

    done with Section 2

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v1 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 
 @Injectable()
 export class TasksService {
@@ -11,8 +12,22 @@ export class TasksService {
         return this.tasks;
     }
 
+    getTasksWithFilters(filterDto: GetTasksFilterDto): Task[] {
+        const { status, search } = filterDto;
+        let tasks = this.getAllTasks();
+        if (status) {
+            tasks = tasks.filter(task => task.status === status);
+        }
+        if (search) {
+            tasks = tasks.filter(task => 
+                task.title.includes(search) ||
+                task.description.includes(search)
+            );
+        }
+        return tasks;
+    }
+
     getTaskById(id: string): Task {
-        console.log(this.tasks.find(task => task.id === id));
         return this.tasks.find(task => task.id === id);
     }
 
@@ -32,5 +47,10 @@ export class TasksService {
     deleteTask(id: string): Task[] {
         this.tasks = this.tasks.filter(task => task.id !== id);
         return this.getAllTasks();
+    }
+
+    updateTaskStatus(id: string, status: TaskStatus): Task {
+        this.getTaskById(id).status = status;
+        return this.getTaskById(id);
     }
 }
